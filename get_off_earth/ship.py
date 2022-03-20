685bd355a965bce18df5f9fcfa5e471efa68ce6c
jpcaulfi <joecaulfield29@yahoo.com>
2022-03-20 15:06:44 -0400

    updated ship objects

--- get_off_earth/ship.py
+++ get_off_earth/ship.py
@@ -4,55 +4,55 @@
 class Ship:
     def __init__(self, 
                 id, 
-                name, 
                 planet_id, 
                 planet_name,
                 distance,
                 ship_type_id, 
                 ship_tye_model_name, 
+                max_capacity,
+                name, 
                 trip_time, 
                 trip_danger, 
-                max_capacity,
                 passengers):
         self.id = id
-        self.name = name
         self.planet_id = planet_id
         self.planet_name = planet_name
         self.distance = distance # Light-years
         self.ship_type_id = ship_type_id
         self.ship_tye_model_name = ship_tye_model_name
+        self.max_capacity = max_capacity
+        self.name = name
         self.trip_time = trip_time # Years
         self.trip_danger = trip_danger # High, Medium, Low
-        self.max_capacity = max_capacity
         self.passengers = passengers
 
 
 class ShipDAO:
     def __init__(self, 
                 id, 
-                name, 
                 planet_id, 
                 ship_type_id, 
+                name, 
                 trip_time, 
-                trip_danger, 
-                max_capacity):
+                trip_danger):
         self.id = id
-        self.name = name
         self.planet_id = planet_id
         self.ship_type_id = ship_type_id
+        self.name = name
         self.trip_time = trip_time # Years
         self.trip_danger = trip_danger # High, Medium, Low
-        self.max_capacity = max_capacity
 
 
 class ShipType:
     def __init__(self, 
                 id, 
                 model_name, 
-                hyperspeed_rating):
+                hyperspeed_rating,
+                max_capacity):
         self.id = id
         self.model_name = model_name
         self.hyperspeed_rating = hyperspeed_rating # trip_time = distance/hyperspeed_rating
+        self.max_capacity = max_capacity
 
 
 @app.route('/ships')
